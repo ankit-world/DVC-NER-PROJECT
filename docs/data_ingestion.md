e6c6a25cdbaa98fe22b9917480ac9f0810f0d0ab
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-02-28 12:56:06 +0530

    new structure updated

---
+++ docs/data_ingestion.md
@@ -0,0 +1,8 @@
+Data Ingested from Hugging Face Library
+
+Library needed - from datasets import load_dataset
+
+dataset: 
+    
+    name: xtreme
+    subset: PAN-X.en
--- init_setup.sh
+++ init_setup.sh
@@ -4,14 +4,14 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "create folder and file structure"
-for DIR in data_ingestion data_preparation data_validation model model_eval model_trainer
-do  
-    echo [$(date)]: "Creating", "src/NER_"$DIR
-    mkdir -p "src/NER_"$DIR
-    echo [$(date)]: "Creating __init__.py inside above folders"
-    touch "src/NER_"$DIR/"__init__.py" "src/NER_"$DIR/$DIR".py"
-    echo "from src.NER_"$DIR"."$DIR" import *" > "src/NER_"$DIR/"__init__.py"
-done
+# for DIR in data_ingestion data_preparation data_validation model model_eval model_trainer
+# do  
+#     echo [$(date)]: "Creating", "src/NER_"$DIR
+#     mkdir -p "src/NER_"$DIR
+#     echo [$(date)]: "Creating __init__.py inside above folders"
+#     touch "src/NER_"$DIR/"__init__.py" "src/NER_"$DIR/$DIR".py"
+#     echo "from src.NER_"$DIR"."$DIR" import *" > "src/NER_"$DIR/"__init__.py"
+# done
 
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt -q
